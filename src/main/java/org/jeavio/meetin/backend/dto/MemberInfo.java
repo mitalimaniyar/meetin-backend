66dfca4c52fd2bcb4a020f387f7110304b18b0a5
mitalimaniyar <mitali@jeavio.com>
2019-06-26 17:23:12 +0530

    Override equals method to avoid multiple entries of one user of different team

--- src/main/java/org/jeavio/meetin/backend/dto/MemberInfo.java
+++ src/main/java/org/jeavio/meetin/backend/dto/MemberInfo.java
@@ -12,17 +12,43 @@ public class MemberInfo extends UserInfo {
 		this.team = team;
 	}
 
-	public MemberInfo(UserInfo user,String team) {
-		super(user.getId(),user.getEmpId(),user.getName(),null,user.getEmail());
+	public MemberInfo(UserInfo user, String team) {
+		super(user.getId(), user.getEmpId(), user.getName(), null, user.getEmail());
 		this.team = team;
 	}
-	
+
 	public String getTeam() {
 		return team;
 	}
 
 	public void setTeam(String team) {
 		this.team = team;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (!(obj instanceof MemberInfo)) {
+			return false;
+		}
+		MemberInfo other = (MemberInfo) obj;
+		if (team == null && other.team != null) {
+			return false;
+		} else
+			return true;
 	}
 
 }
--- src/main/java/org/jeavio/meetin/backend/dto/UserInfo.java
+++ src/main/java/org/jeavio/meetin/backend/dto/UserInfo.java
@@ -35,4 +35,60 @@ public class UserInfo {
 	public String getEmail() {
 		return email;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((empId == null) ? 0 : empId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof UserInfo)) {
+			return false;
+		}
+		UserInfo other = (UserInfo) obj;
+		if (email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!email.equals(other.email)) {
+			return false;
+		}
+		if (empId == null) {
+			if (other.empId != null) {
+				return false;
+			}
+		} else if (!empId.equals(other.empId)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
